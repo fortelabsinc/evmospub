5535625c1582735a3aff257c3b05169165b043d9
Riccardo Zanei <zanei.riccardo2812@gmail.com>
2022-09-21 16:34:57 +0200

    imported customtransfer

---
+++ x/customtransfer/keeper/keeper.go
@@ -0,0 +1,63 @@
+package keeper
+
+import (
+	"fmt"
+
+	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/evmos/evmos/v6/x/customtransfer/types"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/ignite/cli/ignite/pkg/cosmosibckeeper"
+)
+
+type (
+	Keeper struct {
+		*cosmosibckeeper.Keeper
+		cdc        codec.BinaryCodec
+		storeKey   sdk.StoreKey
+		memKey     sdk.StoreKey
+		paramstore paramtypes.Subspace
+		transferKeeper types.TransferKeeper
+		bankKeeper types.BankKeeper
+	}
+)
+
+func NewKeeper(
+	cdc codec.BinaryCodec,
+	storeKey,
+	memKey sdk.StoreKey,
+	ps paramtypes.Subspace,
+	channelKeeper cosmosibckeeper.ChannelKeeper,
+	portKeeper cosmosibckeeper.PortKeeper,
+	scopedKeeper cosmosibckeeper.ScopedKeeper,
+	transferKeeper types.TransferKeeper,
+	bankKeeper types.BankKeeper,
+) *Keeper {
+	// set KeyTable if it has not already been set
+	if !ps.HasKeyTable() {
+		ps = ps.WithKeyTable(types.ParamKeyTable())
+	}
+
+	return &Keeper{
+		Keeper: cosmosibckeeper.NewKeeper(
+			types.PortKey,
+			storeKey,
+			channelKeeper,
+			portKeeper,
+			scopedKeeper,
+		),
+		cdc:        cdc,
+		storeKey:   storeKey,
+		memKey:     memKey,
+		paramstore: ps,
+		transferKeeper: transferKeeper,
+		bankKeeper: bankKeeper,
+	}
+}
+
+func (k Keeper) Logger(ctx sdk.Context) log.Logger {
+	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
+}
---
+++ x/customtransfer/keeper/msg_server_order.go
@@ -0,0 +1,43 @@
+package keeper
+
+import (
+	"context"
+	"github.com/evmos/evmos/v6/x/customtransfer/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
+)
+
+func (k msgServer) SendOrder(goCtx context.Context, msg *types.MsgSendOrder) (*types.MsgSendOrderResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: logic before transmitting the packet
+
+	// Construct the packet
+	var packet types.OrderPacketData
+
+	packet.Receiver = msg.Receiver
+	packet.Pair = msg.Pair
+	packet.Amount = msg.Token.Amount.String()
+	packet.Denom = msg.Token.Denom
+	packet.Direction = msg.Direction
+	packet.Price = msg.Price
+	packet.Threshold = msg.Threshold
+	packet.Senderaddress = msg.Senderaddress
+
+	// Transmit the packet
+	err := k.TransmitOrderPacket(
+		ctx,
+		packet,
+		msg.Token,
+		msg.Port,
+		msg.ChannelID,
+		clienttypes.ZeroHeight(),
+		msg.TimeoutTimestamp,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.MsgSendOrderResponse{}, nil
+}
